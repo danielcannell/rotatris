d6f562f7c0d909a546cdf86f31d6691e04ea8960
Daniel Cannell <danielcannell@googlemail.com>
2022-10-01 19:09:07 +0100

    Proper rotation code

--- Block.gd
+++ Block.gd
@@ -6,6 +6,9 @@ const BLOCK_SIZE: int = 32
 var squares: Array; # of (int, int)
 var color: int;  # Colors
 
+var shape: int = -1
+var rot: int = 0
+
 # Global grid coordinate of the local (0, 0) position
 var coord := [0, 0]
 
@@ -22,101 +25,79 @@ var _slide_time := 1.0
 
 
 const SHAPES_ROTATIONS = {
-	"line_h": [[0,0], [1,0], [2,0], [3,0]],
-	"line_v": [[0,0], [0,1], [0,2], [0,3]],
-	"box": [[0,0], [0,1], [1,1], [1,0]],
-	"j_h_down": [[-1, 0], [0, 0], [1, 0], [1, 1]],
-	"j_v_left": [[0, -1], [0, 0], [0, 1], [-1, 1]],
-	"j_h_up": [[-1, 0], [0, 0], [1, 0], [-1, -1]],
-	"j_v_right": [[0, -1], [0, 0], [0, 1], [1, -1]],
-	"l_h_down": [[-1, 0], [0, 0], [1, 0], [-1, 1]],
-	"l_v_left": [[0, -1], [0, 0], [0, 1], [-1, -1]],
-	"l_h_up": [[-1, 0], [0, 0], [1, 0], [1, -1]],
-	"l_v_right": [[0, -1], [0, 0], [0, 1], [1, 1]],
-	"s_h":  [[-1, 1], [0, 1], [0, 0], [1, 0]],
-	"s_v":  [[0, 0], [0, -1], [1, 0], [1, 1]],
-	"t_down": [[-1, 0], [0, 0], [1, 0], [0, -1]],
-	"t_left": [[0, -1], [0, 0], [0, 1], [-1, 0]],
-	"t_up": [[-1, 0], [0, 0], [1, 0], [0, 1]],
-	"t_right": [[0, -1], [0, 0], [0, 1], [1, 0]],
-	"z_h":  [[-1, 0], [0, 0], [0, 1], [1, 1]],
-	"z_v":  [[0, -1], [0, 0], [1, 0], [1, 1]],
+    "line_h": [[0,0], [1,0], [2,0], [3,0]],
+    "line_v": [[0,0], [0,1], [0,2], [0,3]],
+    "box": [[0,0], [0,1], [1,1], [1,0]],
+    "j_h_down": [[-1, 0], [0, 0], [1, 0], [1, 1]],
+    "j_v_left": [[0, -1], [0, 0], [0, 1], [-1, 1]],
+    "j_h_up": [[-1, 0], [0, 0], [1, 0], [-1, -1]],
+    "j_v_right": [[0, -1], [0, 0], [0, 1], [1, -1]],
+    "l_h_down": [[-1, 0], [0, 0], [1, 0], [-1, 1]],
+    "l_v_left": [[0, -1], [0, 0], [0, 1], [-1, -1]],
+    "l_h_up": [[-1, 0], [0, 0], [1, 0], [1, -1]],
+    "l_v_right": [[0, -1], [0, 0], [0, 1], [1, 1]],
+    "s_h":  [[-1, 1], [0, 1], [0, 0], [1, 0]],
+    "s_v":  [[0, 0], [0, -1], [1, 0], [1, 1]],
+    "t_down": [[-1, 0], [0, 0], [1, 0], [0, -1]],
+    "t_left": [[0, -1], [0, 0], [0, 1], [-1, 0]],
+    "t_up": [[-1, 0], [0, 0], [1, 0], [0, 1]],
+    "t_right": [[0, -1], [0, 0], [0, 1], [1, 0]],
+    "z_h":  [[-1, 0], [0, 0], [0, 1], [1, 1]],
+    "z_v":  [[0, -1], [0, 0], [1, 0], [1, 1]],
 }
 
 const SHAPE_ROT_LOOKUP = {
-	Globals.DefaultShapes.I: {
-		0: SHAPES_ROTATIONS["line_v"],
-		1: SHAPES_ROTATIONS["line_h"],
-		2: SHAPES_ROTATIONS["line_v"],
-		3: SHAPES_ROTATIONS["line_h"],
-	},
-	Globals.DefaultShapes.O: {
-		0: SHAPES_ROTATIONS["box"],
-		1: SHAPES_ROTATIONS["box"],
-		2: SHAPES_ROTATIONS["box"],
-		3: SHAPES_ROTATIONS["box"],
-	},
-	Globals.DefaultShapes.J: {
-		0: SHAPES_ROTATIONS["j_h_down"],
-		1: SHAPES_ROTATIONS["j_v_left"],
-		2: SHAPES_ROTATIONS["j_h_up"],
-		3: SHAPES_ROTATIONS["j_v_right"],
-	},
-	Globals.DefaultShapes.L: {
-		0: SHAPES_ROTATIONS["l_h_down"],
-		1: SHAPES_ROTATIONS["l_v_left"],
-		2: SHAPES_ROTATIONS["l_h_up"],
-		3: SHAPES_ROTATIONS["l_v_right"],
-	},
-	Globals.DefaultShapes.S: {
-		0: SHAPES_ROTATIONS["s_h"],
-		1: SHAPES_ROTATIONS["s_v"],
-		2: SHAPES_ROTATIONS["s_h"],
-		3: SHAPES_ROTATIONS["s_v"],
-	},
-	Globals.DefaultShapes.Z: {
-		0: SHAPES_ROTATIONS["z_h"],
-		1: SHAPES_ROTATIONS["z_v"],
-		2: SHAPES_ROTATIONS["z_h"],
-		3: SHAPES_ROTATIONS["z_v"],
-	},
-	Globals.DefaultShapes.T: {
-		0: SHAPES_ROTATIONS["t_down"],
-		1: SHAPES_ROTATIONS["t_left"],
-		2: SHAPES_ROTATIONS["t_up"],
-		3: SHAPES_ROTATIONS["t_right"],
-	},
+    Globals.DefaultShapes.I: {
+        0: SHAPES_ROTATIONS["line_v"],
+        1: SHAPES_ROTATIONS["line_h"],
+        2: SHAPES_ROTATIONS["line_v"],
+        3: SHAPES_ROTATIONS["line_h"],
+    },
+    Globals.DefaultShapes.O: {
+        0: SHAPES_ROTATIONS["box"],
+        1: SHAPES_ROTATIONS["box"],
+        2: SHAPES_ROTATIONS["box"],
+        3: SHAPES_ROTATIONS["box"],
+    },
+    Globals.DefaultShapes.J: {
+        0: SHAPES_ROTATIONS["j_h_down"],
+        1: SHAPES_ROTATIONS["j_v_left"],
+        2: SHAPES_ROTATIONS["j_h_up"],
+        3: SHAPES_ROTATIONS["j_v_right"],
+    },
+    Globals.DefaultShapes.L: {
+        0: SHAPES_ROTATIONS["l_h_down"],
+        1: SHAPES_ROTATIONS["l_v_left"],
+        2: SHAPES_ROTATIONS["l_h_up"],
+        3: SHAPES_ROTATIONS["l_v_right"],
+    },
+    Globals.DefaultShapes.S: {
+        0: SHAPES_ROTATIONS["s_h"],
+        1: SHAPES_ROTATIONS["s_v"],
+        2: SHAPES_ROTATIONS["s_h"],
+        3: SHAPES_ROTATIONS["s_v"],
+    },
+    Globals.DefaultShapes.Z: {
+        0: SHAPES_ROTATIONS["z_h"],
+        1: SHAPES_ROTATIONS["z_v"],
+        2: SHAPES_ROTATIONS["z_h"],
+        3: SHAPES_ROTATIONS["z_v"],
+    },
+    Globals.DefaultShapes.T: {
+        0: SHAPES_ROTATIONS["t_down"],
+        1: SHAPES_ROTATIONS["t_left"],
+        2: SHAPES_ROTATIONS["t_up"],
+        3: SHAPES_ROTATIONS["t_right"],
+    },
 }
 
 
 # shape is Globals.DefaultShapes
 func from_shape(shape: int):
     self.color = randi() % Globals.Colors.size()
-
-    if shape == Globals.DefaultShapes.I:
-        self.squares = [[0,0], [0,1], [0,2], [0,3]]
-    elif shape == Globals.DefaultShapes.J:
-        self.squares = [[0,0], [0,1], [1,1], [2,1]]
-    elif shape == Globals.DefaultShapes.L:
-        self.squares = [[0,0], [0,1], [1,0], [2,0]]
-    elif shape == Globals.DefaultShapes.O:
-        self.squares = [[0,0], [0,1], [1,1], [1,0]]
-    elif shape == Globals.DefaultShapes.Z:
-        self.squares = [[0,0], [0,1], [1,1], [1,2]]
-    elif shape == Globals.DefaultShapes.S:
-        self.squares = [[1,0], [1,1], [0,1], [0,2]]
-    elif shape == Globals.DefaultShapes.T:
-        self.squares = [[0,0], [1,0], [1,1], [2,0]]
-    else:
-        assert(false)
-
-    # Super hacky code!!! REMOVE ME
-    var centre := [0, 0]
-    for square in self.squares:
-        centre = [centre[0] + square[0], centre[1] + square[1]]
-    centre = [int(centre[0] / 4), int(centre[1] / 4)]
-    for i in range(len(self.squares)):
-        self.squares[i] = [self.squares[i][0] - centre[0], self.squares[i][1] - centre[1]]
+    self.shape = shape
+    self.rot = randi() % 4
+    self.squares = SHAPE_ROT_LOOKUP[self.shape][self.rot]
 
 
 func random():
@@ -217,10 +198,8 @@ func move(grid: Dictionary, step: Array, smooth := true):
 
 
 func try_rotate(grid: Dictionary, rotate: int):
-    var new_squares := [] + self.squares
-    for i in range(rotate):
-        for j in range(len(new_squares)):
-            new_squares[j] = [-new_squares[j][1], new_squares[j][0]]
+    var new_rot = (self.rot + 1) % 4
+    var new_squares = SHAPE_ROT_LOOKUP[self.shape][new_rot]
 
     for square in new_squares:
         var c := [self.coord[0] + square[0], self.coord[1] + square[1]]
@@ -241,5 +220,6 @@ func try_rotate(grid: Dictionary, rotate: int):
 
     self.erase_grid(grid)
     self.squares = new_squares
+    self.rot = new_rot
     self.update_shape()
     self.fill_grid(grid)
