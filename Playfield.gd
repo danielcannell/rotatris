782f4d585b81ecf3848b5c14e1e39d32c0270bfd
Daniel Cannell <danielcannell@googlemail.com>
2022-10-01 15:33:44 +0100

    Spawn periodically

--- Playfield.gd
+++ Playfield.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 
-const TIME_TO_MOVE_1_SQUARE = 1
-const MOVES_PER_GRAVITY_CHANGE = int(3 / TIME_TO_MOVE_1_SQUARE)
+const TIME_TO_MOVE_1_SQUARE := 0.5
+const MOVES_PER_GRAVITY_CHANGE := int(3 / TIME_TO_MOVE_1_SQUARE)
+const GRID_WIDTH := 10
 
 
 # All blocks currently in play
@@ -17,16 +18,13 @@ var gravity_counter := 0
 # Current gravity vector
 var gravity: Array = [0, 1]
 
+# TODO: Should spawn when falling tetronimo lands
+var time_until_spawn := 0.0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
     self.blocks = []
-
-    for x in range(7):
-        var block: Node2D = Block.new()
-        block.from_shape(x)
-        add_child(block)
-        self.blocks.append(block)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -42,12 +40,26 @@ func _process(delta):
             self.gravity_counter = 0
             self.rotate_gravity()
 
+    self.time_until_spawn -= delta
+    if self.time_until_spawn < 0.0:
+        spawn_random_block()
+
+        self.time_until_spawn = 5.0
+
 
 # Update the grid coordinates of all blocks
 func update_block_positions():
     for block in blocks:
-        block.coord = [block.coord[0] + self.gravity[0], block.coord[1] + self.gravity[1]]
+        var new_coord = [block.coord[0] + self.gravity[0], block.coord[1] + self.gravity[1]]
+        block.coord = new_coord
 
 
 func rotate_gravity():
     self.gravity = [self.gravity[1], -self.gravity[0]]
+
+
+func spawn_random_block():
+    var block: Node2D = Block.new()
+    block.random()
+    add_child(block)
+    self.blocks.append(block)
