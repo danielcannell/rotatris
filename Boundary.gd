0de7e3e7ddf9e21d9ca1da94dd40279cd4d2ba00
Jamie Wood <jamie.wood70@ntlworld.com>
2022-10-01 19:28:58 +0100

    Fix boundary position

--- Boundary.gd
+++ Boundary.gd
@@ -20,11 +20,11 @@ func make_wall(origin: Vector2, vec: Vector2, total: int):
 
 func update_shape():
     var half = Globals.GRID_HALF_WIDTH
-    make_wall(center + Vector2(-half, half+1) * Globals.BLOCK_SIZE, Vector2(1, 0), Globals.GRID_WIDTH+2)
-    make_wall(center + Vector2(-half, -half) * Globals.BLOCK_SIZE, Vector2(1, 0), Globals.GRID_WIDTH+2)
+    make_wall(center + Vector2(-half - 1, half) * Globals.BLOCK_SIZE, Vector2(1, 0), Globals.GRID_WIDTH+2)
+    make_wall(center + Vector2(-half - 1, -half - 1) * Globals.BLOCK_SIZE, Vector2(1, 0), Globals.GRID_WIDTH+2)
 
-    make_wall(center + Vector2(half+1, -half) * Globals.BLOCK_SIZE, Vector2(0, 1), Globals.GRID_WIDTH+2)
-    make_wall(center + Vector2(-half, -half) * Globals.BLOCK_SIZE, Vector2(0, 1), Globals.GRID_WIDTH+2)
+    make_wall(center + Vector2(half, -half - 1) * Globals.BLOCK_SIZE, Vector2(0, 1), Globals.GRID_WIDTH+2)
+    make_wall(center + Vector2(-half - 1, -half - 1) * Globals.BLOCK_SIZE, Vector2(0, 1), Globals.GRID_WIDTH+2)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
