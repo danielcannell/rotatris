1937bdec7ced3ce5f29187ba348f4249e9d33bcb
Robert McGregor <rmcgregor1990@gmail.com>
2022-10-02 20:41:46 +0100

    square dissolve shader

--- Square.gd
+++ Square.gd
@@ -1,4 +1,9 @@
 extends Node2D
+onready var sprite = $Sprite
+
+var _is_dissolving = false
+var burn_duration = 4.0;
+var _time = 0.0;
 
 
 func _init():
@@ -10,3 +15,16 @@ func set_color(color: int):
 
 func set_frame(frame: int):
     $Sprite.frame = frame
+
+
+func dissolve():
+    _is_dissolving = true
+
+
+func _process(delta):
+    if _is_dissolving:
+        _time += delta;
+        sprite.material.set_shader_param("burn_position", ((_time / burn_duration) * 2) - 1 )
+        if _time > burn_duration:
+            _is_dissolving = false
+            queue_free()
